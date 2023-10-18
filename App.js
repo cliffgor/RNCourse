6356119d606843649ced10c644411fca6cf5448e
cliffgor <clierozigy@gmail.com>
2023-10-18 21:38:51 +0300

    Work on building the course app as i learn RN basics

--- App.js
+++ App.js
@@ -1,31 +1,19 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={{ backgroundColor: 'red', width: 100, height: 100 }}>
-
+    <View >
+      <View>
+        <TextInput placeholder='Your Course Goal' />
+        <Button title='Add Goal' />
       </View>
-      <Text style={styles.welcomeText}>Welcome to RNCourses</Text>
-      <Text style={{ margin: 16, borderWidth: 2, borderColor: 'green', padding: 15 }}>Another Welcome Message</Text>
-      <Button title='Open the new test' />
-
+      <View>
+        <Text>Course Goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 
-  welcomeText: {
-    margin: 16,
-    borderWidth: 2,
-    borderColor: 'red',
-    padding: 15
-  }
 });
