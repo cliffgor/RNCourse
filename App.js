077f58836815b0330c7fa3b54aae883a5e97bf5f
cliffgor <clierozigy@gmail.com>
2023-10-19 23:58:06 +0300

    Add flex and few design attributes to the app, also add state to show what is being entered into the text input to show on the console when the onPress handler is passed clicking the button

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
+import {useState} from 'react';
+
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  
+  function goalInputHandler(enteredText) {
+    setEnteredGoal(enteredText);
+  }
+
+  function addGoalHandler() {
+    console.log(enteredGoal);
+   }
+
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your Course Goal' />
-        <Button title='Add Goal' />
+        <TextInput style={styles.textInput} placeholder='Your Course Goal' onChangeText={goalInputHandler} />
+        <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>Course Goals...</Text>
       </View>
     </View>
@@ -16,17 +29,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    flex: 1,
+    padding: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: '#cccccc'
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 10,
     padding: 10
+  },
+  goalsContainer: {
+    flex: 4,
   }
 });
